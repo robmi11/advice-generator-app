6c37a786454e2c49c96e6cc36146154660d23df5
robmi11 <robert.mizianty@gmail.com>
2022-02-21 22:41:58 +0100

    App ready.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
 import Card from "./components/Card";
 
 const App = () => {
+  const [advice, setAdvice] = useState({});
+  const [newAdvice, setNewAdvice] = useState(true);
+
+  useEffect(() => {
+    const getAdvice = async () => {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const { slip } = await response.json();
+      setAdvice(slip);
+    };
+    getAdvice();
+  }, [newAdvice]);
+
+  const getNewAdvice = () => {
+    setNewAdvice(!newAdvice);
+  };
+
   return (
     <div className="container">
-      <Card />
+      <Card getNewAdvice={getNewAdvice} advice={advice} />
     </div>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,15 @@
 import Divider from "../images/divider.svg";
 import Icon from "../images/icon-dice.svg";
 
-const Card = () => {
+const Card = ({ getNewAdvice, advice }) => {
   return (
     <main className="card">
-      <p className="card__text">Advaice #117</p>
-      <h2 className="card__advice">
-        If you are ever in doubt about whether or not to wash your hair: Wash
-        it.
-      </h2>
+      <p className="card__text">Advaice #{advice.id}</p>
+      <h2 className="card__advice">{advice.advice}</h2>
       <div className="card__img">
         <img src={Divider} alt="" />
       </div>
-      <div className="card__icon">
+      <div className="card__icon" onClick={getNewAdvice}>
         <img src={Icon} alt="" />
       </div>
     </main>
